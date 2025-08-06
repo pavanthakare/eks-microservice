486eb382ad79b90b909e33290bc9e9ca94067a0e
pavanthakare <120007564+pavanthakare@users.noreply.github.com>
2025-08-06 16:42:49 +0530

    Update app.test.js

--- test/app.test.js
+++ test/app.test.js
@@ -1,10 +1,20 @@
-const request = require('supertest');
 const app = require('../app');
+const request = require('supertest');
+
+let server;
+
+beforeAll(() => {
+  server = app.listen(4000); // use a test port
+});
+
+afterAll((done) => {
+  server.close(done);
+});
 
 describe('GET /', () => {
   it('should return 200 and hello message', async () => {
-    const res = await request(app).get('/');
+    const res = await request(server).get('/');
     expect(res.statusCode).toEqual(200);
-    expect(res.text).toContain('Hello');
+    expect(res.text).toBe('Hello from Microservice A!');
   });
 });
